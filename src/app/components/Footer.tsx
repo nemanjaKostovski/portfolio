756d578cbc96871e8c0a04f211e53a9dc134a33c
Nemanja Kostovski <n.kostovski@almserbia.local>
2023-07-23 06:40:35 +0200

    footer fix for build

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   const year: number = new Date().getFullYear();
   return (
-    <div className='flex justify-center border-t w-9/12'>
+    <div className='flex w-9/12 justify-center border-t'>
       <div className='h-10'></div>
-      <p> Nemanja Kostovski, All Rights Reserved &copy; {year} </p>
+      <p> Nemanja Kostovski, All Rights Reserved `&copy;` {year} </p>
     </div>
   );
 }

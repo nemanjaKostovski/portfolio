ba8630e991d7fa5ad915853eeb9ef3210077cbdd
Nemanja <nemanjakostovski@gmail.com>
2023-07-31 15:45:35 +0200

    Fixed navbar for mobile light

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -54,12 +54,12 @@ export default function Navbar() {
           } relative right-4 h-8 w-8 cursor-pointer text-3xl sm:hidden`}
           onClick={handleToggle}
         >
-          <div className="-mt-0.5 h-1 w-8 rounded bg-white transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-white before:transition-all before:duration-500 before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-white after:transition-all after:duration-500 after:content-['']"></div>
+          <div className="-mt-0.5 h-1 w-8 rounded bg-black transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-black before:transition-all before:duration-500 before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-black after:transition-all after:duration-500 after:content-[''] dark:bg-white dark:before:bg-white dark:after:bg-white"></div>
         </button>
       </div>
 
       <section
-        className={`justify-content-center top-68 absolute z-10 mt-8 w-full origin-top flex-col bg-black text-5xl
+        className={`justify-content-center top-68 absolute z-10 mt-8 w-full origin-top flex-col bg-inherit text-5xl dark:bg-black
         ${toggle ? 'flex' : 'hidden'}
           `}
       >

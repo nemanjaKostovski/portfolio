501bedbfa9afd2592389bf3005a8fb8733909dcc
Nemanja <nemanjakostovski@gmail.com>
2023-09-11 14:58:00 +0200

    added streaming project

--- src/app/projectData.ts
+++ src/app/projectData.ts
@@ -1,4 +1,13 @@
 export const projectsData = {
+  streaming: {
+    name: 'StreamingMax',
+    image: '/spacex-logo.png',
+    tech: 'NextJS TS Tailwind NextAuth',
+    shortDescription: 'Built with the movieDB API',
+    link: 'https://streaming-sigma-red.vercel.app/',
+    longDescription:
+      'This website is built with the movieDB API. This website is built according to the requirements that can be found in the article on this link: https://dev.to/devland/5-real-world-projects-for-practicing-with-nextjs-55g7 . It is inspired after HBOMax web application. You can access each movie and play trailers if available.',
+  },
   spacex: {
     name: 'SpaceX',
     image: '/spacex-logo.png',
@@ -6,7 +15,7 @@ export const projectsData = {
     shortDescription: 'Built with the SpaceX API',
     link: 'https://next-spacex-nemanjakostovski.vercel.app/',
     longDescription:
-      'This website is build on SpaceX API. In the beginning it shows basic info about SpaceX and then all the launches that have patch in its API. With it comes search input for fast narrowing of available launches. Each launch is clickable, where some additional info about launch can be red if available, and all newer launches have a carousel with images from launch site.',
+      'This website is built with the SpaceX API. In the beginning it shows basic info about SpaceX and then all the launches that have patch in its API. With it comes search input for fast narrowing of available launches. Each launch is clickable, where some additional info about launch can be red if available, and all newer launches have a carousel with images from launch site.',
   },
   networktigers: {
     name: 'NetworkTigers',
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,6 +7,13 @@ export default function Projects() {
       <h2 className='p-16 text-2xl'>Check out my awesome projects!</h2>
       <section className='mb-32 grid gap-x-2 gap-y-2 pb-16 pt-16 md:grid-cols-2 lg:grid-cols-3'>
         <ProjectCard
+          link={projectsData.streaming.link}
+          name={projectsData.streaming.name}
+          image={projectsData.streaming.image}
+          description={projectsData.streaming.longDescription}
+          tech={projectsData.streaming.tech}
+        />
+        <ProjectCard
           link={projectsData.spacex.link}
           name={projectsData.spacex.name}
           image={projectsData.spacex.image}
